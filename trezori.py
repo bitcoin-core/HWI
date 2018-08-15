f743fc9ee163991d6246443a458251694fa368a0
Andrew Chow <achow101-github@achow101.com>
2018-08-15 16:36:09 -0700

    Trezor stuff. it probably works. maybe not, idk

--- trezori.py
+++ trezori.py
@@ -1,12 +1,12 @@
 # Trezor interaction script
 
 from hwi import HardwareWalletClient
-from trezorlib.transport_hid import HidTransport
 from trezorlib.client import TrezorClient as Trezor
-from trezorlib.types_trezor_pb2 import TxInputType, TxOutputType, TransactionType,\
-    SPENDADDRESS, SPENDWITNESS, SPENDMULTISIG, SPENDP2SHWITNESS, PAYTOADDRESS,\
-    PAYTOOPRETURN, PAYTOWITNESS, PAYTOP2SHWITNESS, EXTERNAL
-from trezorlib.tx_api import TxApi
+from trezorlib.transport import get_transport
+from trezorlib import coins
+from trezorlib import messages as proto
+from trezorlib import protobuf
+from trezorlib import tools
 from base58 import get_xpub_fingerprint, decode, to_address
 
 import binascii
@@ -26,13 +26,7 @@ class TrezorClient(HardwareWalletClient):
     def __init__(self, device, path):
         super(TrezorClient, self).__init__(device)
         device.close()
-        devices = HidTransport.enumerate()
-        self.client = None
-        for d in devices:
-            if d[0] == path:
-                transport = HidTransport(d)
-                self.client = Trezor(transport)
-                break
+        self.client = Trezor(transport=get_transport("hid:"+path.decode()))
 
         # if it wasn't able to find a client, throw an error
         if not self.client:
@@ -42,7 +36,7 @@ class TrezorClient(HardwareWalletClient):
     # Must return a dict with the xpub
     # Retrieves the public key at the specified BIP 32 derivation path
     def get_pubkey_at_path(self, path):
-        expanded_path = self.client.expand_path(path)
+        expanded_path = tools.parse_path(path)
         output = self.client.get_public_node(expanded_path)
         return json.dumps({'xpub':output.xpub})
 
@@ -51,13 +45,19 @@ class TrezorClient(HardwareWalletClient):
     def sign_tx(self, tx):
 
         # Get this devices master key fingerprint
-        master_key = self.client.get_public_node([])
+        master_key = self.client.get_public_node([0])
         master_fp = get_xpub_fingerprint(master_key.xpub)
 
         # Prepare inputs
         inputs = []
         for psbt_in, txin in zip(tx.inputs, tx.tx.vin):
-            txinputtype = TxInputType()
+            txinputtype = proto.TxInputType()
+
+            # Set the input stuff
+            txinputtype.prev_hash = txin.prevout.hash
+            txinputtype.prev_index = txin.prevout.n
+            txinputtype.sequence = txin.nSequence
+
             # Check for 1 key
             if len(psbt_in.hd_keypaths) == 1:
                 # Is this key ours
@@ -65,16 +65,11 @@ class TrezorClient(HardwareWalletClient):
                     # Set the keypath
                     txinputtype.address_n.extend(psbt_in.hd_keypaths.values()[0])
                 else:
-                    txinputtype.script_type = external
-
-                    # Set the input stuff
-                    txinputtype.prev_hash = txin.prevout.hash
-                    txinputtype.prev_index = txin.prevout.n
-                    txinputtype.sequence = txin.nSequence
+                    txinputtype.script_type = proto.InputScriptType.EXTERNAL
 
                     # Set spend type
-                    if txinputtype.script_type != EXTERNAL and psbt_in.non_witness_utxo:
-                        txinputtype.script_type = SPENDADDRESS
+                    if txinputtype.script_type != proto.InputScriptType.EXTERNAL and psbt_in.non_witness_utxo:
+                        txinputtype.script_type = proto.InputScriptType.SPENDADDRESS
 
             # Check for multisig (more than 1 key)
             elif len(psbt_in.hd_keypaths) > 1:
@@ -84,11 +79,6 @@ class TrezorClient(HardwareWalletClient):
 
                         # Set the keypaths
                         txinputtype.address_n.extend(keypath)
-
-                        # Set the input stuff
-                        txinputtype.prev_hash = txin.prevout.hash
-                        txinputtype.prev_index = txin.prevout.n
-                        txinputtype.sequence = txin.nSequence
 
                         # Set multisig
                         multisig = proto_types.MultisigRedeemScriptType(
@@ -103,14 +93,14 @@ class TrezorClient(HardwareWalletClient):
 
                 # Set spend type
                 if psbt_in.non_witness_utxo:
-                    txinputtype.script_type = SPENDMULTISIG
+                    txinputtype.script_type = proto.InputScriptType.SPENDMULTISIG
 
             if psbt_in.witness_utxo:
                 # Check if the output is p2sh
                 if psbt_in.witness_utxo.is_p2sh():
-                    txinputtype.script_type = SPENDP2SHWITNESS
+                    txinputtype.script_type = proto.InputScriptType.SPENDP2SHWITNESS
                 else:
-                    txinputtype.script_type = SPENDWITNESS
+                    txinputtype.script_type = proto.InputScriptType.SPENDWITNESS
 
             # Set the amount
             if psbt_in.non_witness_utxo:
@@ -124,13 +114,15 @@ class TrezorClient(HardwareWalletClient):
         # prepare outputs
         outputs = []
         for out in tx.tx.vout:
-            txoutput = TxOutputType()
+            txoutput = proto.TxOutputType()
             if out.is_p2pkh:
                 txoutput.address = to_address(out.scriptPubKey[2:22], b"\x00")
                 txoutput.amount = out.nValue
-                txoutput.script_type = PAYTOADDRESS
+                txoutput.script_type = proto.OutputScriptType.PAYTOADDRESS
             elif out.is_p2sh:
                 txoutput.address = to_address(out.scriptPubKey[3:23], b"\x05")
+                txoutput.amount = out.nValue
+                txoutput.script_type = proto.OutputScriptType.PAYTOADDRESS
             else:
                 # TODO: Figure out what to do here. for now, just break
                 break
@@ -139,7 +131,7 @@ class TrezorClient(HardwareWalletClient):
             outputs.append(txoutput)
 
         # Sign the transaction
-        self.client.set_tx_api()
+        self.client.set_tx_api(coins.tx_api['Bitcoin'])
         signed_tx = self.client.sign_tx("Bitcoin", inputs, outputs, tx.tx.nVersion, tx.tx.nLockTime)
         print(signed_tx)
 
