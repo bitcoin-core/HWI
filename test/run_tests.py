ef816628e14619eb245dd3336b0e3db50eabf807
Andrew Chow <achow101-github@achow101.com>
2019-05-22 13:49:29 -0400

    Switch to Trezor monorepo

--- test/run_tests.py
+++ test/run_tests.py
@@ -19,7 +19,7 @@ from test_udevrules import TestUdevRulesInstaller
 parser = argparse.ArgumentParser(description='Setup the testing environment and run automated tests')
 trezor_group = parser.add_mutually_exclusive_group()
 trezor_group.add_argument('--no_trezor', help='Do not run Trezor test with emulator', action='store_true')
-trezor_group.add_argument('--trezor', help='Path to Trezor emulator.', default='work/trezor-mcu/firmware/trezor.elf')
+trezor_group.add_argument('--trezor', help='Path to Trezor emulator.', default='work/trezor-firmware/legacy/firmware/trezor.elf')
 coldcard_group = parser.add_mutually_exclusive_group()
 coldcard_group.add_argument('--no_coldcard', help='Do not run Coldcard test with simulator', action='store_true')
 coldcard_group.add_argument('--coldcard', help='Path to Coldcard simulator.', default='work/firmware/unix/headless.py')
