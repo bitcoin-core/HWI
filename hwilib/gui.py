3d530cad2d2b1d9e51c7ba20ee55fdb30271bc7e
Andrew Chow <achow101-github@achow101.com>
2019-12-16 18:06:01 -0500

    Implement SendPinDialog and sendpin_button

--- hwilib/gui.py
+++ hwilib/gui.py
@@ -6,13 +6,15 @@ from . import commands
 
 try:
     from .ui.ui_mainwindow import Ui_MainWindow
+    from .ui.ui_sendpindialog import Ui_SendPinDialog
     from .ui.ui_setpassphrasedialog import Ui_SetPassphraseDialog
 except ImportError:
     print('Could not import UI files, did you run contrib/generate-ui.sh')
     exit(-1)
 
-from PySide2.QtWidgets import QApplication, QDialog, QMainWindow
-from PySide2.QtCore import Slot
+from PySide2.QtGui import QRegExpValidator
+from PySide2.QtWidgets import QApplication, QDialog, QLineEdit, QMainWindow
+from PySide2.QtCore import QRegExp, Signal, Slot
 
 class SetPassphraseDialog(QDialog):
     def __init__(self):
@@ -23,6 +25,48 @@ class SetPassphraseDialog(QDialog):
 
         self.ui.passphrase_lineedit.setFocus()
 
+class SendPinDialog(QDialog):
+    pin_sent_success = Signal()
+
+    def __init__(self, client):
+        super(SendPinDialog, self).__init__()
+        self.ui = Ui_SendPinDialog()
+        self.ui.setupUi(self)
+        self.setWindowTitle('Send Pin')
+        self.client = client
+        self.ui.pin_lineedit.setFocus()
+        self.ui.pin_lineedit.setValidator(QRegExpValidator(QRegExp("[1-9]+"), None))
+        self.ui.pin_lineedit.setEchoMode(QLineEdit.Password)
+
+        self.ui.p1_button.clicked.connect(self.button_clicked(1))
+        self.ui.p2_button.clicked.connect(self.button_clicked(2))
+        self.ui.p3_button.clicked.connect(self.button_clicked(3))
+        self.ui.p4_button.clicked.connect(self.button_clicked(4))
+        self.ui.p5_button.clicked.connect(self.button_clicked(5))
+        self.ui.p6_button.clicked.connect(self.button_clicked(6))
+        self.ui.p7_button.clicked.connect(self.button_clicked(7))
+        self.ui.p8_button.clicked.connect(self.button_clicked(8))
+        self.ui.p9_button.clicked.connect(self.button_clicked(9))
+
+        self.accepted.connect(self.sendpindialog_accepted)
+        commands.prompt_pin(self.client)
+
+    def button_clicked(self, number):
+        @Slot()
+        def button_clicked_num():
+            self.ui.pin_lineedit.setText(self.ui.pin_lineedit.text() + str(number))
+        return button_clicked_num
+
+    @Slot()
+    def sendpindialog_accepted(self):
+        pin = self.ui.pin_lineedit.text()
+
+        # Send the pin
+        commands.send_pin(self.client, pin)
+        self.client.close()
+        self.client = None
+        self.pin_sent_success.emit()
+
 class HWIQt(QMainWindow):
     def __init__(self):
         super(HWIQt, self).__init__()
@@ -32,16 +76,22 @@ class HWIQt(QMainWindow):
 
         self.devices = []
         self.client = None
+        self.device_info = {}
         self.passphrase = ''
         self.current_dialog = None
 
         self.ui.enumerate_refresh_button.clicked.connect(self.refresh_clicked)
         self.ui.setpass_button.clicked.connect(self.show_setpassphrasedialog)
+        self.ui.sendpin_button.clicked.connect(self.show_sendpindialog)
 
-        self.ui.enumerate_combobox.currentIndexChanged.connect(self.get_device_info)
+        self.ui.enumerate_combobox.currentIndexChanged.connect(self.get_client_and_device_info)
 
     @Slot()
     def refresh_clicked(self):
+        if self.client:
+            self.client.close()
+            self.client = None
+
         self.devices = commands.enumerate(self.passphrase)
         self.ui.enumerate_combobox.currentIndexChanged.disconnect()
         self.ui.enumerate_combobox.clear()
@@ -52,7 +102,7 @@ class HWIQt(QMainWindow):
                 fingerprint = dev['fingerprint']
             dev_str = '{} fingerprint:{} path:{}'.format(dev['model'], fingerprint, dev['path'])
             self.ui.enumerate_combobox.addItem(dev_str)
-        self.ui.enumerate_combobox.currentIndexChanged.connect(self.get_device_info)
+        self.ui.enumerate_combobox.currentIndexChanged.connect(self.get_client_and_device_info)
 
     @Slot()
     def show_setpassphrasedialog(self):
@@ -66,17 +116,19 @@ class HWIQt(QMainWindow):
         self.current_dialog = None
 
     @Slot()
-    def get_device_info(self, index):
+    def get_client_and_device_info(self, index):
         self.ui.sendpin_button.setEnabled(False)
         if index == 0:
             return
 
         # Get the client
-        dev = self.devices[index - 1]
-        self.client = commands.get_client(dev['model'], dev['path'], self.passphrase)
+        self.device_info = self.devices[index - 1]
+        self.client = commands.get_client(self.device_info['model'], self.device_info['path'], self.passphrase)
+        self.get_device_info()
 
+    def get_device_info(self):
         # Enable the sendpin button if it's a trezor and it needs it
-        if dev['needs_pin_sent']:
+        if self.device_info['needs_pin_sent']:
             self.ui.sendpin_button.setEnabled(True)
             return
         else:
@@ -88,6 +140,20 @@ class HWIQt(QMainWindow):
 
         self.ui.keypool_textedit.setPlainText(json.dumps(keypool, indent=2))
         self.ui.desc_textedit.setPlainText(json.dumps(descriptors, indent=2))
+
+    @Slot()
+    def show_sendpindialog(self):
+        self.current_dialog = SendPinDialog(self.client)
+        self.current_dialog.pin_sent_success.connect(self.sendpindialog_accepted)
+        self.current_dialog.exec_()
+
+    @Slot()
+    def sendpindialog_accepted(self):
+        self.current_dialog = None
+
+        curr_index = self.ui.enumerate_combobox.currentIndex()
+        self.refresh_clicked()
+        self.ui.enumerate_combobox.setCurrentIndex(curr_index)
 
 def main():
     app = QApplication()
