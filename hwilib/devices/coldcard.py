51eac02552bcde9891fdf7bb0083ca820a828d0c
Andrew Chow <achow101-github@achow101.com>
2019-12-17 18:44:50 -0500

    coldcard: do multiple passes when the same key is in an input multiple times

--- hwilib/devices/coldcard.py
+++ hwilib/devices/coldcard.py
@@ -6,7 +6,8 @@ from ..errors import ActionCanceledError, BadArgumentError, DeviceBusyError, Dev
 from .ckcc.client import ColdcardDevice, COINKITE_VID, CKCC_PID
 from .ckcc.protocol import CCProtocolPacker, CCBusyError, CCProtoError, CCUserRefused
 from .ckcc.constants import MAX_BLK_LEN, AF_P2WPKH, AF_CLASSIC, AF_P2WPKH_P2SH
-from ..base58 import xpub_main_2_test
+from ..base58 import get_xpub_fingerprint, xpub_main_2_test
+from ..serializations import PSBT
 from hashlib import sha256
 
 import base64
@@ -68,53 +69,72 @@ class ColdcardClient(HardwareWalletClient):
     def sign_tx(self, tx):
         self.device.check_mitm()
 
-        # Get psbt in hex and then make binary
-        fd = io.BytesIO(base64.b64decode(tx.serialize()))
+        # Get this devices master key fingerprint
+        xpub = self.device.send_recv(CCProtocolPacker.get_xpub('m/0\''), timeout=None)
+        master_fp = get_xpub_fingerprint(xpub)
 
-        # learn size (portable way)
-        sz = fd.seek(0, 2)
-        fd.seek(0)
+        # For multisigs, we may need to do multiple passes if we appear in an input multiple times
+        passes = 1
+        for psbt_in in tx.inputs:
+            our_keys = 0
+            for key in psbt_in.hd_keypaths.keys():
+                keypath = psbt_in.hd_keypaths[key]
+                if keypath[0] == master_fp and key not in psbt_in.partial_sigs:
+                    our_keys += 1
+            if our_keys > passes:
+                passes = our_keys
 
-        left = sz
-        chk = sha256()
-        for pos in range(0, sz, MAX_BLK_LEN):
-            here = fd.read(min(MAX_BLK_LEN, left))
-            if not here:
+        for i in range(0, passes):
+            # Get psbt in hex and then make binary
+            fd = io.BytesIO(base64.b64decode(tx.serialize()))
+
+            # learn size (portable way)
+            sz = fd.seek(0, 2)
+            fd.seek(0)
+
+            left = sz
+            chk = sha256()
+            for pos in range(0, sz, MAX_BLK_LEN):
+                here = fd.read(min(MAX_BLK_LEN, left))
+                if not here:
+                    break
+                left -= len(here)
+                result = self.device.send_recv(CCProtocolPacker.upload(pos, sz, here))
+                assert result == pos
+                chk.update(here)
+
+            # do a verify
+            expect = chk.digest()
+            result = self.device.send_recv(CCProtocolPacker.sha256())
+            assert len(result) == 32
+            if result != expect:
+                raise DeviceFailureError("Wrong checksum:\nexpect: %s\n   got: %s" % (b2a_hex(expect).decode('ascii'), b2a_hex(result).decode('ascii')))
+
+            # start the signing process
+            ok = self.device.send_recv(CCProtocolPacker.sign_transaction(sz, expect), timeout=None)
+            assert ok is None
+            if self.device.is_simulator:
+                self.device.send_recv(CCProtocolPacker.sim_keypress(b'y'))
+
+            print("Waiting for OK on the Coldcard...", file=sys.stderr)
+
+            while 1:
+                time.sleep(0.250)
+                done = self.device.send_recv(CCProtocolPacker.get_signed_txn(), timeout=None)
+                if done is None:
+                    continue
                 break
-            left -= len(here)
-            result = self.device.send_recv(CCProtocolPacker.upload(pos, sz, here))
-            assert result == pos
-            chk.update(here)
 
-        # do a verify
-        expect = chk.digest()
-        result = self.device.send_recv(CCProtocolPacker.sha256())
-        assert len(result) == 32
-        if result != expect:
-            raise DeviceFailureError("Wrong checksum:\nexpect: %s\n   got: %s" % (b2a_hex(expect).decode('ascii'), b2a_hex(result).decode('ascii')))
+            if len(done) != 2:
+                raise DeviceFailureError('Failed: %r' % done)
 
-        # start the signing process
-        ok = self.device.send_recv(CCProtocolPacker.sign_transaction(sz, expect), timeout=None)
-        assert ok is None
-        if self.device.is_simulator:
-            self.device.send_recv(CCProtocolPacker.sim_keypress(b'y'))
+            result_len, result_sha = done
 
-        print("Waiting for OK on the Coldcard...", file=sys.stderr)
+            result = self.device.download_file(result_len, result_sha, file_number=1)
 
-        while 1:
-            time.sleep(0.250)
-            done = self.device.send_recv(CCProtocolPacker.get_signed_txn(), timeout=None)
-            if done is None:
-                continue
-            break
-
-        if len(done) != 2:
-            raise DeviceFailureError('Failed: %r' % done)
-
-        result_len, result_sha = done
-
-        result = self.device.download_file(result_len, result_sha, file_number=1)
-        return {'psbt': base64.b64encode(result).decode()}
+            tx = PSBT()
+            tx.deserialize(base64.b64encode(result).decode())
+        return {'psbt': tx.serialize()}
 
     # Must return a base64 encoded string with the signed message
     # The message can be any string. keypath is the bip 32 derivation path for the key to sign with
