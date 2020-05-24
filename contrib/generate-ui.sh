edcee233c6b26b23331aaea3b719d75d585cae3d
Andrew Chow <achow101-github@achow101.com>
2020-05-24 19:06:53 -0400

    fix generate-ui.sh for GNU sed
    This was changed to work with BSD sed (used by macOS) which breaks GNU
    sed. This should work for both.

--- contrib/generate-ui.sh
+++ contrib/generate-ui.sh
@@ -5,6 +5,6 @@ for file in *.ui
 do
     gen_file=ui_`echo $file| cut -d. -f1`.py
     pyside2-uic $file -o $gen_file
-    sed -i '' -e 's/raise()/raise_()/g' $gen_file
+    sed -i'' -e 's/raise()/raise_()/g' $gen_file
 done
 popd
