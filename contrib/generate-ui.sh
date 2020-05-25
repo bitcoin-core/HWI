4504cffdaf71eeec1293b8da8faa9500f751a168
benk10 <ben.kaufman10@gmail.com>
2020-05-25 08:35:42 +0300

    Update generate-ui.sh

--- contrib/generate-ui.sh
+++ contrib/generate-ui.sh
@@ -5,6 +5,10 @@ for file in *.ui
 do
     gen_file=ui_`echo $file| cut -d. -f1`.py
     pyside2-uic $file -o $gen_file
-    sed -i '' -e 's/raise()/raise_()/g' $gen_file
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        sed -i '' -e 's/raise()/raise_()/g' $gen_file
+    else
+        sed -i 's/raise()/raise_()/g' $gen_file
+    fi
 done
 popd
