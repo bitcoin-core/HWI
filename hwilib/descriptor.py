a25baf858d4b4ecff0d0baa3fd59e0951d9690ef
benk10 <ben.kaufman10@gmail.com>
2020-06-10 22:04:31 +0300

    Support multisig descriptors in `displayaddress`

--- hwilib/descriptor.py
+++ hwilib/descriptor.py
@@ -50,7 +50,21 @@ def AddChecksum(desc):
     return desc + "#" + DescriptorChecksum(desc)
 
 class Descriptor:
-    def __init__(self, origin_fingerprint, origin_path, base_key, path_suffix, testnet, sh_wpkh, wpkh):
+    def __init__(
+        self,
+        origin_fingerprint,
+        origin_path,
+        base_key,
+        path_suffix,
+        testnet,
+        sh_wpkh=None,
+        wpkh=None,
+        sh=None,
+        sh_wsh=None,
+        wsh=None,
+        multisig_M=None,
+        multisig_N=None
+    ):
         self.origin_fingerprint = origin_fingerprint
         self.origin_path = origin_path
         self.path_suffix = path_suffix
@@ -58,9 +72,14 @@ class Descriptor:
         self.testnet = testnet
         self.sh_wpkh = sh_wpkh
         self.wpkh = wpkh
+        self.sh = sh
+        self.sh_wsh = sh_wsh
+        self.wsh = wsh
+        self.multisig_M = multisig_M
+        self.multisig_N = multisig_N
         self.m_path = None
 
-        if origin_path:
+        if origin_path and not isinstance(origin_path, list):
             self.m_path_base = "m" + origin_path
             self.m_path = "m" + origin_path + (path_suffix or "")
 
@@ -68,11 +87,16 @@ class Descriptor:
     def parse(cls, desc, testnet=False):
         sh_wpkh = None
         wpkh = None
+        sh = None
+        sh_wsh = None
+        wsh = None
         origin_fingerprint = None
         origin_path = None
         base_key_and_path_match = None
         base_key = None
         path_suffix = None
+        multisig_M = None
+        multisig_N = None
 
         # Check the checksum
         check_split = desc.split('#')
@@ -92,31 +116,70 @@ class Descriptor:
             sh_wpkh = True
         elif desc.startswith("wpkh("):
             wpkh = True
+        elif desc.startswith("sh(wsh("):
+            sh_wsh = True
+        elif desc.startswith("wsh("):
+            wsh = True
+        elif desc.startswith("sh("):
+            sh = True
 
-        origin_match = re.search(r"\[(.*)\]", desc)
-        if origin_match:
-            origin = origin_match.group(1)
-            match = re.search(r"^([0-9a-fA-F]{8})(\/.*)", origin)
-            if match:
-                origin_fingerprint = match.group(1)
-                origin_path = match.group(2)
-                # Replace h with '
-                origin_path = origin_path.replace('h', '\'')
-
-            base_key_and_path_match = re.search(r"\[.*\](\w+)([\/\)][\d'\/\*]*)", desc)
-        else:
-            base_key_and_path_match = re.search(r"\((\w+)([\/\)][\d'\/\*]*)", desc)
-
-        if base_key_and_path_match:
-            base_key = base_key_and_path_match.group(1)
-            path_suffix = base_key_and_path_match.group(2)
-            if path_suffix == ")":
-                path_suffix = None
-        else:
-            if origin_match is None:
+        if sh or sh_wsh or wsh:
+            if 'multi(' not in desc:
+                # only multisig scripts are supported
                 return None
+            # get the list of keys only
+            keys = desc.split(',', 1)[1].split(')', 1)[0].split(',')
+            if 'sortedmulti' in desc:
+                keys.sort(key=lambda x: x if ']' not in x else x.split(']')[1])
+            multisig_M = desc.split(',')[0].split('(')[-1]
+            multisig_N = len(keys)
+        else:
+            keys = [desc.split('(')[-1].split(')', 1)[0]]
 
-        return cls(origin_fingerprint, origin_path, base_key, path_suffix, testnet, sh_wpkh, wpkh)
+        descriptors = []
+        for key in keys:
+            origin_match = re.search(r"\[(.*)\]", key)
+            if origin_match:
+                origin = origin_match.group(1)
+                match = re.search(r"^([0-9a-fA-F]{8})(\/.*)", origin)
+                if match:
+                    origin_fingerprint = match.group(1)
+                    origin_path = match.group(2)
+                    # Replace h with '
+                    origin_path = origin_path.replace('h', '\'')
+
+                base_key_and_path_match = re.search(r"\[.*\](\w+)([\d'\/\*]*)", key)
+            else:
+                base_key_and_path_match = re.search(r"(\w+)([\d'\/\*]*)", key)
+
+            if base_key_and_path_match:
+                base_key = base_key_and_path_match.group(1)
+                path_suffix = base_key_and_path_match.group(2)
+                if path_suffix == '':
+                    path_suffix = None
+            else:
+                if origin_match is None:
+                    return None
+
+            descriptors.append(cls(origin_fingerprint, origin_path, base_key, path_suffix, testnet, sh_wpkh, wpkh, sh, sh_wsh, wsh))
+        if len(descriptors) == 1:
+            return descriptors[0]
+        else:
+            # for multisig scripts save as lists all keypaths fields
+            return cls(
+                [descriptor.origin_fingerprint for descriptor in descriptors],
+                [descriptor.origin_path for descriptor in descriptors],
+                [descriptor.base_key for descriptor in descriptors],
+                [descriptor.path_suffix for descriptor in descriptors],
+                testnet,
+                sh_wpkh,
+                wpkh,
+                sh,
+                sh_wsh,
+                wsh,
+                multisig_M,
+                multisig_N
+            )
 
     def serialize(self):
         descriptor_open = 'pkh('
@@ -129,6 +192,9 @@ class Descriptor:
         elif self.sh_wpkh:
             descriptor_open = 'sh(wpkh('
             descriptor_close = '))'
+        elif self.sh or self.sh_wsh or self.wsh:
+            # serialize multisig descriptor is not supported yet.
+            return None
 
         if self.origin_fingerprint and self.origin_path:
             origin = '[' + self.origin_fingerprint + self.origin_path + ']'
