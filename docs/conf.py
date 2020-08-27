b209f369778aae0b441cb096e4afb7a2ea3717b4
Andrew Chow <achow101-github@achow101.com>
2020-08-27 16:29:35 -0400

    docstring errors.py and add to api-usage.rst

--- docs/conf.py
+++ docs/conf.py
@@ -61,3 +61,9 @@ html_theme = 'alabaster'
 
 # Autodoc config to include type hints in the description
 autodoc_typehints = "description"
+
+# Order the autodoc members by type
+autodoc_member_order = "bysource"
+
+# Show both class and init docstring
+autoclass_content = "both"
