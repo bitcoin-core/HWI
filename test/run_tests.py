dd33cd9ab58e86695482083ff61585d1d580fe05
Andrew Chow <achow101-github@achow101.com>
2021-01-22 16:29:51 -0500

    Change run_tests.py --bitbox to --bitbox01

--- test/run_tests.py
+++ test/run_tests.py
@@ -39,21 +39,22 @@ keepkey_group.add_argument('--no-keepkey', dest='keepkey', help='Do not run Keep
 keepkey_group.add_argument('--keepkey', dest='keepkey', help='Run Keepkey test with emulator', action='store_true')
 
 dbb_group = parser.add_mutually_exclusive_group()
-dbb_group.add_argument('--no_bitbox', dest='bitbox', help='Do not run Digital Bitbox test with simulator', action='store_false')
-dbb_group.add_argument('--bitbox', dest='bitbox', help='Run Digital Bitbox test with simulator', action='store_true')
+dbb_group.add_argument('--no_bitbox01', dest='bitbox01', help='Do not run Digital Bitbox test with simulator', action='store_false')
+dbb_group.add_argument('--bitbox01', dest='bitbox01', help='Run Digital Bitbox test with simulator', action='store_true')
 
 parser.add_argument('--trezor-1-path', dest='trezor_1_path', help='Path to Trezor 1 emulator', default='work/trezor-firmware/legacy/firmware/trezor.elf')
 parser.add_argument('--trezor-t-path', dest='trezor_t_path', help='Path to Trezor T emulator', default='work/trezor-firmware/core/emu.sh')
 parser.add_argument('--coldcard-path', dest='coldcard_path', help='Path to Coldcar simulator', default='work/firmware/unix/headless.py')
 parser.add_argument('--keepkey-path', dest='keepkey_path', help='Path to Keepkey emulator', default='work/keepkey-firmware/bin/kkemu')
-parser.add_argument('--bitbox-path', dest='bitbox_path', help='Path to Digital Bitbox simulator', default='work/mcu/build/bin/simulator')
+parser.add_argument('--bitbox01-path', dest='bitbox01_path', help='Path to Digital Bitbox simulator', default='work/mcu/build/bin/simulator')
 parser.add_argument('--ledger-path', dest='ledger_path', help='Path to Ledger emulator', default='work/speculos/speculos.py')
 
 parser.add_argument('--all', help='Run tests on all existing simulators', default=False, action='store_true')
 parser.add_argument('--bitcoind', help='Path to bitcoind', default='work/bitcoin/src/bitcoind')
 parser.add_argument('--interface', help='Which interface to send commands over', choices=['library', 'cli', 'bindist', 'stdin'], default='library')
 
-parser.set_defaults(trezor_1=None, trezor_t=None, coldcard=None, keepkey=None, bitbox=None, ledger=None)
+parser.set_defaults(trezor_1=None, trezor_t=None, coldcard=None, keepkey=None, bitbox01=None, ledger=None)
+
 args = parser.parse_args()
 
 # Run tests
@@ -74,7 +75,7 @@ if args.all:
     args.trezor_t = True if args.trezor_t is None else args.trezor_t
     args.coldcard = True if args.coldcard is None else args.coldcard
     args.keepkey = True if args.keepkey is None else args.keepkey
-    args.bitbox = True if args.bitbox is None else args.bitbox
+    args.bitbox01 = True if args.bitbox01 is None else args.bitbox01
     args.ledger = True if args.ledger is None else args.ledger
 else:
     # Default all false unless overridden
@@ -82,15 +83,15 @@ else:
     args.trezor_t = False if args.trezor_t is None else args.trezor_t
     args.coldcard = False if args.coldcard is None else args.coldcard
     args.keepkey = False if args.keepkey is None else args.keepkey
-    args.bitbox = False if args.bitbox is None else args.bitbox
+    args.bitbox01 = False if args.bitbox01 is None else args.bitbox01
     args.ledger = False if args.ledger is None else args.ledger
 
-if args.trezor_1 or args.trezor_t or args.coldcard or args.ledger or args.keepkey or args.bitbox:
+if args.trezor_1 or args.trezor_t or args.coldcard or args.ledger or args.keepkey or args.bitbox01:
     # Start bitcoind
     rpc, userpass = start_bitcoind(args.bitcoind)
 
-    if success and args.bitbox:
-        success &= digitalbitbox_test_suite(args.bitbox_path, rpc, userpass, args.interface)
+    if success and args.bitbox01:
+        success &= digitalbitbox_test_suite(args.bitbox01_path, rpc, userpass, args.interface)
     if success and args.coldcard:
         success &= coldcard_test_suite(args.coldcard_path, rpc, userpass, args.interface)
     if success and args.trezor_1:
