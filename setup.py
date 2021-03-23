a30d672e56ee142d68a399f8ecd95af7d809e287
Marko Bencun <mbencun+pgp@gmail.com>
2021-03-23 18:34:50 +0100

    bitbox02: update to v5.3.0
    Updated pyproject.toml and setup.py, then: `poetry update bitbox02
    --lock`.
    
    - Antiklepto active also when signing messages.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ package_data = \
 modules = \
 ['hwi', 'hwi-qt']
 install_requires = \
-['bitbox02>=5.2.0,<6.0.0',
+['bitbox02>=5.3.0,<6.0.0',
  'ecdsa>=0,<1',
  'hidapi>=0,<1',
  'libusb1>=1.7,<2.0',
